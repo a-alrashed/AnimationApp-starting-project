9b89cd56ec6ffaacc9ba6f8e15f989ee2866028f
Azzam R Alrashed <a.alrashed@safcsp.org.sa>
2020-12-10 08:31:21 +0300

    worked on Auth View controller animations

---
+++ AnimationApp/Controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  AnimationApp
+//
+//  Created by Azzam R Alrashed on 09/12/2020.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
